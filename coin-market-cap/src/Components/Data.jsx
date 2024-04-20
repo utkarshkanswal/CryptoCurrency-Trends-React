711c2d538b5124a41539bca4ed8bdb69009045a9
Utkarsh Kumar <utkarsh.k@innovaccer.com>
2024-04-21 02:27:05 +0530

    update(CryptoCurrency-Trends):Added crypto currency trends

---
+++ coin-market-cap/src/Components/Data.jsx
@@ -0,0 +1,21 @@
+
+import Table from './CustomizedTables'
+import Header from './Header'
+
+function Data() {
+    return (
+
+        <div>
+            <Header/>
+            <div style={{marginTop:"18vh",marginLeft:"35vh",marginRight:"35vh"}}>
+                    <Table/>
+            </div>
+            
+
+        </div>
+        
+    );
+}
+
+export default Data;
+
